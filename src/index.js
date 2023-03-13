466caed0deaed774bad49eb5ba7111d599f6e410
Alexandr <you@example.com>
2023-03-13 21:44:34 +0300

    add main text components

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles/style.scss'
+import './styles/fonts.css'
+import './styles/media.css'
+import logo from './images/icons/logo.png'
+import iconTel from './images/icons/iconTel.png'
+import exit from './images/icons/exit.png'
+
+//js
+document.querySelector('.header__logo img').src = logo
+document.querySelector('.header__number img').src = iconTel
+document.querySelector('.main-info__exit-icon img').src = exit
